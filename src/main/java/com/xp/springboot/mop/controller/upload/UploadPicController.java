cf62411a2d90764f547f42516fda7e05d8e03675
peng.xu <peng.xu@mljr.com>
2017-11-24 14:42:43 +0800

    add common

--- src/main/java/com/xp/springboot/mop/controller/upload/UploadPicController.java
+++ src/main/java/com/xp/springboot/mop/controller/upload/UploadPicController.java
@@ -45,54 +45,54 @@ public class UploadPicController {
         model.addAttribute("name", name);
         return "uploadPic";
     }
-    @RequestMapping("/upload/uploadImage")
+//    @RequestMapping("/upload/uploadImage")
 //    @RequestMapping(method = RequestMethod.POST,value="/upload/uploadImage")
-    public ResponseDTO<List<String>> uploadImage(
-            @RequestParam("file") MultipartFile file,
-            HttpServletRequest request){
-        ResponseDTO<List<String>> responseDTO = new ResponseDTO<>();
-        
-        if(file.isEmpty()){
-            logger.info("文件为空");
-            responseDTO.setCode(-1);
-            responseDTO.setMessage("文件为空");
-            return responseDTO;
-        }
-        String fileName = file.getOriginalFilename();
-        if(!(fileName.endsWith(".png") || fileName.endsWith(".jpg")||fileName.endsWith(".jpeg"))) {
-            logger.info("上传图片类型错误");
-            responseDTO.setCode(WebExceptionEnum.DEFAULT_FAIL.getCode());
-            responseDTO.setMessage("只能上传图片");
-            return responseDTO;
-        }
-        List<String> userIdList = new ArrayList<>();
-        String uuid = UUID.randomUUID().toString().replaceAll("-","");
-        String contentType = file.getContentType();
-        String imageName=contentType.substring(contentType.indexOf("/")+1);  
-        String tempFileName=uuid+"."+imageName;  
-        File myFile = null;
-        try {
-            myFile = File.createTempFile("temp", tempFileName);
-        } catch (IOException e1) {
-            logger.info("创建临时文件出现异常",e1);
-            e1.printStackTrace();
-        }
-        logger.info("准备创建文件:{},path:{}",myFile.getName(),myFile.getAbsolutePath());
-        String resultPath = "";
-        try {
-            file.transferTo(myFile);
-            resultPath =  UploadImg.upload(myFile.getAbsolutePath());
-            logger.info("上传文件返回resultPath:{}",resultPath);
-            boolean deleteFlag = myFile.delete();
-            logger.info("删除文件返回状态:{}",deleteFlag);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-       
-        userIdList.add(resultPath);
-        responseDTO.setData(userIdList);
-        return responseDTO;
-    }
+//    public ResponseDTO<List<String>> uploadImage(
+//            @RequestParam("file") MultipartFile file,
+//            HttpServletRequest request){
+//        ResponseDTO<List<String>> responseDTO = new ResponseDTO<>();
+//        
+//        if(file.isEmpty()){
+//            logger.info("文件为空");
+//            responseDTO.setCode(-1);
+//            responseDTO.setMessage("文件为空");
+//            return responseDTO;
+//        }
+//        String fileName = file.getOriginalFilename();
+//        if(!(fileName.endsWith(".png") || fileName.endsWith(".jpg")||fileName.endsWith(".jpeg"))) {
+//            logger.info("上传图片类型错误");
+//            responseDTO.setCode(WebExceptionEnum.DEFAULT_FAIL.getCode());
+//            responseDTO.setMessage("只能上传图片");
+//            return responseDTO;
+//        }
+//        List<String> userIdList = new ArrayList<>();
+//        String uuid = UUID.randomUUID().toString().replaceAll("-","");
+//        String contentType = file.getContentType();
+//        String imageName=contentType.substring(contentType.indexOf("/")+1);  
+//        String tempFileName=uuid+"."+imageName;  
+//        File myFile = null;
+//        try {
+//            myFile = File.createTempFile("temp", tempFileName);
+//        } catch (IOException e1) {
+//            logger.info("创建临时文件出现异常",e1);
+//            e1.printStackTrace();
+//        }
+//        logger.info("准备创建文件:{},path:{}",myFile.getName(),myFile.getAbsolutePath());
+//        String resultPath = "";
+//        try {
+//            file.transferTo(myFile);
+//            resultPath =  UploadImg.upload(myFile.getAbsolutePath());
+//            logger.info("上传文件返回resultPath:{}",resultPath);
+//            boolean deleteFlag = myFile.delete();
+//            logger.info("删除文件返回状态:{}",deleteFlag);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//       
+//        userIdList.add(resultPath);
+//        responseDTO.setData(userIdList);
+//        return responseDTO;
+//    }
     
 //    @RequestMapping("/uploadPic")
 //    public String uploadPic(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
